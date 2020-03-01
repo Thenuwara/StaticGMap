3fff7f08d59333803388f464659bf9a22b1bcb2d
Thenuwara <thenuwara@m-daq.com>
2020-03-01 10:01:29 +0530

    static g map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  StaticGoogleMap,
+  Marker,
+  Path,
+} from 'react-static-google-map';
 
 function App() {
+  console.log('kkkkkkkkkkkkkk', process.env.REACT_APP_GAPI_KEY)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StaticGoogleMap size="600x600" apiKey={process.env.REACT_APP_GAPI_KEY}>
+        <Marker
+          location={{ lat: 40.737102, lng: -73.990318 }}
+          color="blue"
+          label="P"
+        />
+        <Path
+          points={[
+            '40.737102,-73.990318',
+            '40.749825,-73.987963',
+            '40.752946,-73.987384',
+            '40.755823,-73.986397',
+          ]}
+        />
+      </StaticGoogleMap>
     </div>
   );
 }
